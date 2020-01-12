fce18c766b90415e8e861b6e90b02822876e33d6
Mark <marcus.koh16.01.97@gmail.com>
2020-01-12 23:51:59 +0300

    main script req

---
+++ UCParce.py
@@ -0,0 +1,94 @@
+import requests
+import json
+from bs4 import BeautifulSoup as BS
+import time
+import sys
+import os
+
+
+
+class SessionUC:
+    
+    def __init__(self, username, password):
+        self.__username = username
+        self.__password = password
+        self.session = requests.Session()
+
+        self.payload = {
+            'login': self.__username,
+            'password': self.__password,
+            'remember': 1,
+            '_xfRedirect': '',
+        }
+  
+
+    def auth(self):
+        return self.session.post('https://uc.zone/login/login', data=self.payload)
+
+
+    def get_token(self):
+        token_html = self.session.get('https://uc.zone/login')
+        token_bs = BS(token_html.content, 'html.parser')
+        try:
+            self.token = token_bs.select('input[name=_xfToken]')[0]['value']
+        except IndexError as e:
+            print("Check email and confirm account..or shit happend")
+
+        
+    def wait_new_promo(self):
+        while True:
+            time.sleep(3)
+            promo_html = self.session.get('https://uc.zone/cheat-statuses/games/DotA2/load-promocode')
+            promo_bs = BS(promo_html.content, 'html.parser')
+         
+            current_promocode = promo_bs.select('.gamePromocodeItem.gamePromocode--promocode')[0].text.strip()
+            print(current_promocode)
+
+            if promo_bs.select('.is-not-activated'):
+                self.promocode = promo_bs.select('.gamePromocodeItem.gamePromocode--promocode.is-not-activated')[0]\
+                    .text.strip()
+                break
+
+
+    def activate_promo(self):
+        promo_payload = {
+            'promocode': self.promocode,
+            '_xfToken': self.token
+        }
+        print(promo_payload)
+
+        result = self.session.post('https://uc.zone/account/promocode', data=promo_payload)
+        html_returned = BS(result.content, 'html.parser')
+
+        os.system('play --no-show-progress --null --channels 2 synth %s sine %f' %( 0.2, 400))
+        return html_returned.select('.p-body-pageContent')[0].text.strip()
+
+
+
+if __name__ == '__main__':
+    
+    se = SessionUC("login", "password")
+
+    # login
+    result = se.auth()
+    print(result.url)
+    print(result.history[0])
+    if str(result.history[0]) != '<Response [303]>':
+        print("Invalid email or password")
+        sys.exit()
+
+
+    # get token
+    se.get_token()
+    print(se.token)
+
+    print('Waiting new promo')
+    se.wait_new_promo()
+    print('Activating promocode')
+    result = se.activate_promo()
+
+    print(result)
+
+
+
+
