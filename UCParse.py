74ca5675f3176c298f485df172c01d13e9d4279a
Mark <marcus.koh16.01.97@gmail.com>
2020-06-08 15:02:19 +0300

    add slove captcha

--- UCParse.py
+++ UCParse.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup as BS
 import time
 import sys
 import os
+import random
+import pickle
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -22,43 +24,83 @@ def load_data_from_file():
             result['username'] = data['username']
         if 'password' in data:
             result['password'] = data['password']
+        if 'rucaptcha_key' in data:
+            result['rucaptcha_key'] = data['rucaptcha_key']
 
     except KeyError as error:
         print('Cannot find: %s', error.args[0])
     except Exception as error:
         raise error
     else:
-        return result['username'], result['password']
+        return result['username'], result['password'], result['rucaptcha_key']
 
 class SessionUC:
     
-    def __init__(self, username, password):
+    def __init__(self, username, password, recaptcha_response=None):
         self.__username = username
         self.__password = password
         self.session = requests.Session()
         self.token = None
         self.promocode = None
+        self.g_rec = recaptcha_response
 
         self.payload = {
             'login': self.__username,
             'password': self.__password,
             'remember': 1,
-            '_xfRedirect': '',
+            '_xfRedirect': '/',
+            '_xfToken': ''
         }
 
     def auth(self):
-        r_auth = self.session.post('https://uc.zone/login/login', data=self.payload, verify=False)
-        token_bs = BS(r_auth.text, 'html.parser')
+        if os.path.isfile("cookies"):
+            with open('cookies', 'rb') as file:
+                self.session.cookies.update(pickle.load(file))
+          
+        promo_html = se.session.get('https://uc.zone/cheat-statuses/games/DotA2/load-promocode', verify=False)
+        promo_bs = BS(promo_html.content, 'html.parser')
+        current_promocode = None
+        r_auth = None
         try:
-            self.token = token_bs.select('input[name=_xfToken]')[0]['value']
+            current_promocode = promo_bs.select('.gamePromocodeItem.gamePromocode--promocode')[0].text.strip()
         except IndexError as e:
-            print("Check email and confirm account..or shit happend")
+            print("Login unsucc")
+        
+        if not current_promocode:
+            print("Trying to login")
+            if self.g_rec is None:
+                r_auth = self.session.post('https://uc.zone/login/login', data=self.payload, verify=False)
+            else:
+                data = {
+                    'login': self.__username,
+                    'password': self.__password,
+                    'g-recaptcha-response': self.g_rec,
+                    'remember': 1,
+                    '_xfRedirect': '/',
+                    '_xfToken': ''
+                }
+                r_auth = self.session.post('https://uc.zone/login/login', data=data, verify=False)
+            login_result = self.session.get("https://uc.zone/", verify=False)
+            token_bs = BS(login_result.text, 'html.parser')
+        else:
+            login_result = self.session.get("https://uc.zone/", verify=False)
+            token_bs = BS(login_result.text, 'html.parser')
+        try:
+            print(token_bs.select('input[name=_xfToken]'))
+            self.token = token_bs.select('input[name=_xfToken]')[0]['value']                
+                
+        except IndexError as e:
+            print("Captcha shit")
+            
+        else:
+            with open('cookies', 'wb') as file:
+                pickle.dump(self.session.cookies, file)
         return r_auth
 
     def wait_new_promo(self):
         try:
             while True:
-                time.sleep(3)
+                time.sleep(0.5)
                 promo_html = self.session.get('https://uc.zone/cheat-statuses/games/DotA2/load-promocode')
                 promo_bs = BS(promo_html.content, 'html.parser')
              
@@ -89,19 +131,62 @@ class SessionUC:
         #os.system('play --no-show-progress --null --channels 2 synth %s sine %f' %( 0.2, 400))
         return html_returned.select('.p-body-pageContent')[0].text.strip()
 
+def solve_captcha(rucaptcha_key):
+        try:
+            send_captcha = requests.get(
+                "https://rucaptcha.com/in.php?key=" + rucaptcha_key + "&method=userrecaptcha&googlekey=" + "6LfY1TcUAAAAADxfJBcgupBVijeJO5v-81ZAEvOv" + "&pageurl=https://uc.zone/login/login")
+        except Exception as e:
+            raise e
+        print(send_captcha.text)
+        captcha_id = send_captcha.text.split("|")[1]
+        print(captcha_id)
+     
+        print("rucaptcha id:" + captcha_id)
+        while True:
+            time.sleep(5)
+            print("Ожидаю капчу")
+            try:
+                recived_captcha = requests.get(
+                    "https://rucaptcha.com/res.php?key=" + rucaptcha_key + "&action=get&id=" + captcha_id + "&json=1",
+                    timeout=20)
+            except:
+                continue
+            if (recived_captcha.json()["request"] == "ERROR_CAPTCHA_UNSOLVABLE"):
+                raise Exception("CaptchaUnsolvable")
+            if recived_captcha.json()['status'] == 1:
+                print(recived_captcha.json())
+                recived_captcha_id = captcha_id
+                return recived_captcha.json()['request']
 
 if __name__ == '__main__':
-    
-    username, password = load_data_from_file()
+
+
+    username, password, rucaptcha_key = load_data_from_file()
+    print(rucaptcha_key)
+    print(username + ":" + password)
     se = SessionUC(username, password)
-
-
+    current_promocode = None
+   
     login_result = se.auth()
-    print(login_result.url)
-    print(login_result.history)
-    if not login_result.history:
-        print("Invalid email or password")
-        sys.exit()
+    
+    try:
+        promo_html = se.session.get('https://uc.zone/cheat-statuses/games/DotA2/load-promocode')
+        promo_bs = BS(promo_html.content, 'html.parser')
+        current_promocode = promo_bs.select('.gamePromocodeItem.gamePromocode--promocode')[0].text.strip()
+        print(current_promocode)
+    except Exception as e:
+        print(e)
+    
+    if not current_promocode:
+        print("Captcha")
+        recaptcha_response = solve_captcha(rucaptcha_key)
+        print(recaptcha_response)
+        
+        se = SessionUC(username, password, recaptcha_response)
+        login_result = se.auth()
+        print(login_result.url)
+        print(login_result.history)
+        
 
     print(se.token)
 
